794cb434b9108ba9eb5ff283ff862bb956f5f4a0
rkn2 <rkn2@princeton.edu>
2017-10-20 16:44:33 -0400

    ran it

--- change_filename.py
+++ change_filename.py
@@ -1,9 +1,9 @@
 import glob
 import os
 
-file_path = 'C:/Users/Rebecca Napolitano/Google Drive/Documents/Research/mikehess/palazzo vecchio/2017_9_7_ElementiModels/FoundationModels/ExistingGeometry/bricks/2017_10_19_Existinggeometryfull_brick/'
-originalDate= '9_7'
-newDate = '10_17'
+file_path = 'C:/Users/Rebecca Napolitano/Documents/datafiles/mike/vecchio/existing/2017_10_19_Existinggeometryfull_brick/'
+originalDate= '10_17'
+newDate = '10_19'
 
 os.chdir(file_path)
 fileHandles = glob.glob('2017_*')
