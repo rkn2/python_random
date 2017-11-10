874717b30b01fe7e8560942843327ef97e3d1b5e
rkn2 <rkn2@princeton.edu>
2017-11-10 13:56:27 -0500

    update with names

--- change_filename.py
+++ change_filename.py
@@ -1,15 +1,18 @@
 import glob
 import os
 
-file_path = 'C:/Users/Rebecca Napolitano/Documents/datafiles/mike/vecchio/existing/2017_10_19_Existinggeometryfull_brick/'
-originalDate= '10_17'
-newDate = '10_19'
+file_path = 'C:/Users/Rebecca Napolitano/Documents/datafiles/mike/vecchio/existing/2017_11_10_Existing_brick/'
+originalDate= '10_30'
+newDate = '11_10'
+originalName = 'Existinggeometryfull_finalmodel'
+newName = 'Existing'
 
 os.chdir(file_path)
 fileHandles = glob.glob('2017_*')
 
 for file in fileHandles:
     newFile = file.replace(originalDate,newDate)
+    #newFile = file.replace(originalName, newName)
     os.rename(file, newFile)
     
     
